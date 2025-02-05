c7ff858c9d8d9425caaea0ace702134d67d9a224
Wooyoung Choi <wy.choi@naverlabs.com>
2025-02-05 09:12:07 +0900

    Publish odometry topic based on wheel position

--- src/webots_node.cc
+++ src/webots_node.cc
@@ -1,6 +1,8 @@
+#include <cmath>
 #include <memory>
 
 #include <geometry_msgs/msg/twist.hpp>
+#include <nav_msgs/msg/odometry.hpp>
 #include <rclcpp/rclcpp.hpp>
 
 #include <webots/DistanceSensor.hpp>
@@ -39,8 +41,10 @@ class WebotsRos2Node : public rclcpp::Node {
     cmd_vel_duration_usec_ =
         get_or_default_parameter(this, "cmd_vel_duration_usec", 0);
 
-    auto command_topic =
-        get_or_default_parameter(this, "command_topic", std::string("cmd_vel"));
+    auto command_topic = get_or_default_parameter(this, "command_topic",
+                                                  std::string("/cmd_vel"));
+    auto odom_topic = get_or_default_parameter(this, "odom_topic",
+                                               std::string("/odom/wheel"));
     auto left_wheel_motor = get_or_default_parameter(
         this, "left_wheel_motor", std::string("left wheel motor"));
     auto right_wheel_motor = get_or_default_parameter(
@@ -89,6 +93,7 @@ class WebotsRos2Node : public rclcpp::Node {
     cmd_vel_subscription_ = create_subscription<geometry_msgs::msg::Twist>(
         command_topic, 10,
         std::bind(&WebotsRos2Node::onCommand, this, std::placeholders::_1));
+    odom_publisher_ = create_publisher<nav_msgs::msg::Odometry>(odom_topic, 10);
   }
 
   ~WebotsRos2Node() { delete robot_; }
@@ -113,11 +118,20 @@ class WebotsRos2Node : public rclcpp::Node {
 
   rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr
       cmd_vel_subscription_;
+  rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_publisher_;
+
+  double x_ = 0.0;
+  double y_ = 0.0;
+  double theta_ = 0.0;
 
   double target_rps_left_;
   double target_rps_right_;
-  double wheel_position_left_;
-  double wheel_position_right_;
+  double wheel_velocity_left_ = 0.0;
+  double wheel_velocity_right_ = 0.0;
+  double wheel_position_left_ = 0.0;
+  double wheel_position_right_ = 0.0;
+  double wheel_position_left_prev_ = NAN;
+  double wheel_position_right_prev_ = NAN;
   rclcpp::Time latest_rps_updated_;
 
   void onCommand(const geometry_msgs::msg::Twist::SharedPtr msg) {
@@ -127,6 +141,24 @@ class WebotsRos2Node : public rclcpp::Node {
     target_rps_right_ = (vx + (vth * wheelbase_ / 2.0)) / wheel_radius_;
     latest_rps_updated_ = this->now();
     RCLCPP_INFO(this->get_logger(), "Received command: vx=%f, vth=%f", vx, vth);
+  }
+
+  void publishOdometry() {
+    auto msg = nav_msgs::msg::Odometry();
+    msg.header.stamp = this->now();
+    msg.header.frame_id = "odom";
+    msg.child_frame_id = "base_link";
+
+    msg.pose.pose.position.x = x_;
+    msg.pose.pose.position.y = y_;
+    msg.pose.pose.orientation.z = sin(theta_ / 2.0);
+    msg.pose.pose.orientation.w = cos(theta_ / 2.0);
+
+    msg.twist.twist.linear.x =
+        (wheel_radius_ / 2.0) * (wheel_velocity_left_ + wheel_velocity_right_);
+    msg.twist.twist.angular.z = (wheel_radius_ / wheelbase_) *
+                                (wheel_velocity_right_ - wheel_velocity_left_);
+    odom_publisher_->publish(msg);
   }
 
   void update() {
@@ -152,16 +184,39 @@ class WebotsRos2Node : public rclcpp::Node {
       motor_right_->setVelocity(target_rps_right_);
     }
 
+    double delta_left = 0.0;
+    double delta_right = 0.0;
     if (position_sensor_left_) {
       wheel_position_left_ = position_sensor_left_->getValue();
+      if (std::isnan(wheel_position_left_prev_)) {
+        wheel_position_left_prev_ = wheel_position_left_;
+      }
+      delta_left = wheel_position_left_ - wheel_position_left_prev_;
+
+      wheel_position_left_prev_ = wheel_position_left_;
       RCLCPP_INFO(this->get_logger(), "Left Wheel Position: %.3f",
                   wheel_position_left_);
     }
     if (position_sensor_right_) {
       wheel_position_right_ = position_sensor_right_->getValue();
+      if (std::isnan(wheel_position_right_prev_)) {
+        wheel_position_right_prev_ = wheel_position_right_;
+      }
+      delta_right = wheel_position_right_ - wheel_position_right_prev_;
+      wheel_position_right_prev_ = wheel_position_right_;
       RCLCPP_INFO(this->get_logger(), "Right Wheel Position: %.3f",
                   wheel_position_right_);
     }
+
+    double odom_translation = (delta_left + delta_right) * wheel_radius_ / 2.0;
+    double odom_rotation =
+        (delta_right - delta_left) * wheel_radius_ / wheelbase_;
+    x_ += odom_translation * cos(theta_ + odom_rotation / 2);
+    y_ += odom_translation * sin(theta_ + odom_rotation / 2);
+    theta_ += odom_rotation;
+    wheel_velocity_left_ = delta_left * time_step_;
+    wheel_velocity_right_ = delta_right * time_step_;
+    publishOdometry();
   }
 };
 
